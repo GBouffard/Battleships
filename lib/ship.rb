96d21497270fbfc25e4f77bcbd2f29e7ec9b84a9
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-22 10:54:25 +0100

    ship done

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_reader :size, :floating, :hits_left
+  def initialize(size)
+    @size = size
+    @floating = true
+    @hits_left = size
+  end
+
+  def hit # (ship, location) later
+    @hits_left -= 1
+    sunk?
+  end
+
+  def sunk?
+    @floating = false if @hits_left <= 0
+  end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,7 +1,23 @@
 require 'ship'
 
 describe Ship do
-  ship = Ship.new
-  it '' do
+  let(:ship) { Ship.new(3) }
+
+  it 'has a size' do
+    expect(ship.size).to eq(3)
+  end
+
+  it 'is floating when created' do
+    expect(ship.floating).to be true
+  end
+
+  it 'can be hit' do
+    ship.hit
+    expect(ship.hits_left).to eq 2
+  end
+
+  it 'can be sunk' do
+    3.times { ship.hit }
+    expect(ship.floating).to be false
   end
 end
